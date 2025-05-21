38c40785ab2e0bbce1b1c36130719a813b8169eb
Luiz Teixeira <medeirosteixeiraluizfilipe@gmail.com>
2025-05-20 21:31:43 -0300

    Get Lists

---
+++ src/main/java/com/devsuperior/dslist/services/GameListService.java
@@ -0,0 +1,29 @@
+package com.devsuperior.dslist.services;
+
+import com.devsuperior.dslist.dto.GameDTO;
+import com.devsuperior.dslist.dto.GameListDTO;
+import com.devsuperior.dslist.dto.GameMinDTO;
+import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.entities.GameList;
+import com.devsuperior.dslist.repositories.GameListRepository;
+import com.devsuperior.dslist.repositories.GameRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class GameListService {
+
+    @Autowired
+    private GameListRepository gameListRepositoryRepository;
+
+    @Transactional
+    public List<GameListDTO> findAll(){
+        List<GameList> result = gameListRepositoryRepository.findAll();
+        return result.stream().map(x -> new GameListDTO(x)).toList();
+    }
+
+}
+
